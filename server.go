7a5fd57e8828d66295709eea896e2440251be115
Ludovico Russo <ludus.russo@gmail.com>
2022-02-06 14:49:29 +0100

    fix handler to use websocket

--- server.go
+++ server.go
@@ -13,25 +13,17 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/lifevun-dev/oldg/graph"
 	"github.com/lifevun-dev/oldg/graph/generated"
-	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	c := cors.New(cors.Options{
-		AllowOriginFunc: func(origin string) bool {
-			return true
-		},
-		AllowCredentials: true,
-	})
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	srv.AddTransport(transport.POST{})
 	srv.AddTransport(transport.Websocket{
@@ -44,7 +36,7 @@ func main() {
 	})
 	srv.Use(extension.Introspection{})
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", c.Handler(srv))
+	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
