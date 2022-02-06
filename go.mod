2677f12c8189c5cbcd8c0d6ad1abbe9adc731627
Ludovico Russo <ludus.russo@gmail.com>
2022-02-06 12:12:44 +0100

    try to add cors, not working

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/99designs/gqlgen v0.16.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/rs/cors v1.8.2
 	github.com/vektah/gqlparser/v2 v2.2.0
 )
--- server.go
+++ server.go
@@ -4,16 +4,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/websocket"
 	"github.com/lifevun-dev/oldg/graph"
 	"github.com/lifevun-dev/oldg/graph/generated"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	c := cors.New(cors.Options{
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		AllowCredentials: true,
+	})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -21,8 +33,18 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
+	srv.Use(extension.Introspection{})
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
